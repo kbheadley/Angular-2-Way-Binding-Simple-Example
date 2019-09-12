982d10078965012497caa39027a2aa4afbd551a9
kbheadley <kbhsubscribe@gmail.com>
2019-09-11 22:02:32 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // 2-way binding - step 1:  declare the string to update
+  userName = '';
+
+ 
+
+
+}
